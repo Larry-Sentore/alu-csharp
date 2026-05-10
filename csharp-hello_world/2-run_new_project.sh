65c9ed15ac03a250ad8dd139ed4ec41b748b688a
Larry-Sentore <l.sentore@alustudent.com>
2026-05-10 23:49:06 +0200

    more fixing

--- csharp-hello_world/2-run_new_project.sh
+++ csharp-hello_world/2-run_new_project.sh
@@ -1,5 +1,5 @@
- #!/usr/bin/env bash
- mkdir 2-new_project
- dotnet new console -o 2-new_project
- cd 2-new_project
- dotnet run
+#!/usr/bin/env bash
+mkdir 2-new_project
+dotnet new console -o 2-new_project
+cd 2-new_project
+dotnet run
