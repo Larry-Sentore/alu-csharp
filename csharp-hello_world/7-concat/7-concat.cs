5b4491aa86c4ca3a9b52d515087e0fdf8c71852d
Larry-Sentore <l.sentore@alustudent.com>
2026-05-12 22:52:36 +0200

    quick fix

--- csharp-hello_world/7-concat/7-concat.cs
+++ csharp-hello_world/7-concat/7-concat.cs
@@ -6,7 +6,6 @@ class Program
 	{
 		string str1 = "Holberton";
 		string str2 = "School";
-		Console.Write("Welcome to {0}", str1);
-        Console.WriteLine(" {0}!", str2);
+		Console.WriteLine("Welcome to " + str1 + " " + str2 + "!");
 	}
 }
