ef74f920c85ded9f7605b231742f7b9eb2eea92f
Larry-Sentore <l.sentore@alustudent.com>
2026-05-18 16:58:06 +0200

    did 6-print_combo2

---
+++ csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,11 @@
+﻿string result = "";
+for (int i = 0; i < 10; i++)
+{
+    for (int j = i + 1; j < 10; j++)
+    {
+        if (result != "")
+            result += ", ";
+        result += $"{i}{j}";
+    }
+}
+Console.WriteLine(result);
