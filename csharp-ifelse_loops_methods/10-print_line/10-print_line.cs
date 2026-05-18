d13630ef7ec6ada295982d1a7fccadbe559195b1
Larry-Sentore <l.sentore@alustudent.com>
2026-05-18 17:53:38 +0200

    did 10-print_line

---
+++ csharp-ifelse_loops_methods/10-print_line/10-print_line.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void PrintLine(int n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write("_");
+        }
+        Console.WriteLine();
+    }
+    static void Main(string[] args)
+    {
+        Line.PrintLine(3);
+        Line.PrintLine(0);
+        Line.PrintLine(12);
+        Line.PrintLine(-98);
+    }
+}
