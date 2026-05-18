c69aa2f2d618e143efbce0f60eb826599f64863e
Larry-Sentore <l.sentore@alustudent.com>
2026-05-18 17:45:39 +0200

    did 9-add

---
+++ csharp-ifelse_loops_methods/9-add/9-add.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static int Add(int a, int b)
+    {
+        return a + b;
+    }
+    static void Main(string[] args)
+    {
+        Console.WriteLine("{0}", Number.Add(1, 2));
+        Console.WriteLine("{0}", Number.Add(98, 0));
+        Console.WriteLine("{0}", Number.Add(100, -2));
+    }
+}
