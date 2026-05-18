5dfd1d8f7ba6b552583056a9da9768bbab3f0c95
Larry-Sentore <l.sentore@alustudent.com>
2026-05-18 17:58:48 +0200

    did 11-print_diagonal

---
+++ csharp-ifelse_loops_methods/11-print_diagonal/11-print_diagonal.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class ExecutePrintDiagonal
+{
+    static void PrintDiagonal(int n)
+    {
+        if (n <= 0)
+        {
+            Console.WriteLine();
+            return;
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                Console.Write(" ");
+            }
+            Console.WriteLine("\\");
+        }
+    }
+    static void Main(string[] args)
+    {
+        Line.PrintDiagonal(3);
+        Line.PrintDiagonal(0);
+        Line.PrintDiagonal(12);
+        Line.PrintDiagonal(-98);
+    }
+}
