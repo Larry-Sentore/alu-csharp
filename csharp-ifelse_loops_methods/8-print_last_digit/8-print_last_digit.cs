bb157fe552516ca45bf18e161b6674a0255a51f2
Larry-Sentore <l.sentore@alustudent.com>
2026-05-18 17:33:36 +0200

    did 8-print_last_digits

---
+++ csharp-ifelse_loops_methods/8-print_last_digit/8-print_last_digit.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static int PrintLastDigit(int n)
+    {
+        int lastDigit = Math.Abs(n % 10);
+        Console.WriteLine(lastDigit);
+        return lastDigit;
+    }
+    
+    static void Main(string[] args)
+    {
+        int r;
+
+        Number.PrintLastDigit(98);
+        Number.PrintLastDigit(0);
+        r = Number.PrintLastDigit(-1024);
+        Console.WriteLine(r);
+    }
+}
