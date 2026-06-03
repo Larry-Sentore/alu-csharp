2986a56f3526056b443bb7a1c9661b9345ad0226
Larry-Sentore <l.sentore@alustudent.com>
2026-06-03 09:49:24 +0200

    did 1-element_at.cs

---
+++ csharp-arrays_lists_dictionaries/1-element_at/1-element_at.cs
@@ -0,0 +1,19 @@
+﻿Using System;
+
+class Array
+{
+    
+    public static int elementAt(int[] array, int index)
+    {
+        if (index < 0 || index >= array.Length)
+        {
+            Console.WriteLine("Index is out of range");
+            return -1;
+        }else
+        {
+            Console.WriteLine("Element at index {0} is: {1}", index, array[index]);
+            return array[index];
+        }
+    }
+
+}
