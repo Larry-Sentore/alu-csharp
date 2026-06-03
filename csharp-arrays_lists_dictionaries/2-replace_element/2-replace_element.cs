d6dae44015102cb73979772a80f365d97cb0ee9b
Larry-Sentore <l.sentore@alustudent.com>
2026-06-03 10:08:02 +0200

    did 2-replace_elements

---
+++ csharp-arrays_lists_dictionaries/2-replace_element/2-replace_element.cs
@@ -0,0 +1,15 @@
+﻿Using System;
+
+class Array
+{
+    
+    public static int[] ReplaceElement(int[] array, int index, int n)
+    {
+       
+        Console.WriteLine(array);
+        array[index] = n;
+        Console.WriteLine(array);
+
+    }
+
+}
