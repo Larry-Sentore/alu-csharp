0156ae4ebf8cc51e9c35fe9800ae5456bcc8c5f1
Larry-Sentore <l.sentore@alustudent.com>
2026-06-07 18:57:34 +0200

    quick fix

--- csharp-arrays_lists_dictionaries/1-element_at/1-element_at.cs
+++ csharp-arrays_lists_dictionaries/1-element_at/1-element_at.cs
@@ -1,16 +1,14 @@
 ﻿using System;
 
-class Array
+public class Array
 {
-    
     public static int elementAt(int[] array, int index)
     {
         if (index < 0 || index >= array.Length)
         {
-            Console.WriteLine("Index is out of range");
+            Console.WriteLine("Index out of range");
             return -1;
         }
         return array[index];
     }
-
 }
