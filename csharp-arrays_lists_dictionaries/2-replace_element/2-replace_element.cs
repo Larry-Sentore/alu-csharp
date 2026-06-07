932ed3627dddd0e55de921a612a1d6786e2c3fdb
Larry-Sentore <l.sentore@alustudent.com>
2026-06-07 19:00:56 +0200

    quick fix -2

--- csharp-arrays_lists_dictionaries/2-replace_element/2-replace_element.cs
+++ csharp-arrays_lists_dictionaries/2-replace_element/2-replace_element.cs
@@ -1,20 +1,16 @@
 ﻿using System;
 
-class Array
+public class Array
 {
-    
     public static int[] ReplaceElement(int[] array, int index, int n)
     {
         if (index < 0 || index >= array.Length)
         {
-            return array;
-        }
-        else
-        {
-            array[index] = n;
-            return array;
+            Console.WriteLine("Index out of range");
+            return array; // Return the unchanged array
         }
 
+        array[index] = n; // Replace the element at the given index
+        return array;     // Return the updated array
     }
-
 }
