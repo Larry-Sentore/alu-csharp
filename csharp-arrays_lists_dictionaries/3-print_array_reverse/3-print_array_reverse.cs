769618fa5c8b97a3a0f227eaeb73c064eaa70ef5
Larry-Sentore <l.sentore@alustudent.com>
2026-06-07 19:41:53 +0200

    quick fix -3

--- csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
+++ csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
@@ -1,21 +1,30 @@
 ﻿using System;
 
-class Array
+public class Array
 {
     public static void Reverse(int[] array)
     {
+        
         if (array == null)
+        {
+            Console.WriteLine(); 
             return;
+        }
 
-        int i;
+        
+        if (array.Length == 0)
+        {
+            Console.WriteLine();
+            return;
+        }
 
-        for (i = array.Length - 1; i >= 0; i--)
+        
+        for (int i = array.Length - 1; i >= 0; i--)
         {
             Console.Write(array[i]);
             if (i > 0)
-                Console.Write(" ");
+                Console.Write(" "); 
         }
-        
-        Console.WriteLine();
+        Console.WriteLine(); 
     }
 }
