3d3f93819c56d44f1adf8fbc7dfee169d0e3e740
Larry-Sentore <l.sentore@alustudent.com>
2026-06-28 15:32:53 +0200

    did the TDD

--- csharp-hello_world/2-new_project/obj/Debug/net7.0/2-new_project.AssemblyInfo.cs
+++ csharp-hello_world/2-new_project/obj/Debug/net7.0/2-new_project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("2-new_project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d05f49ac716767116a95aba230f4310dce5d319e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf67e20b324af3564bac086f78f53df598aef25c")]
 [assembly: System.Reflection.AssemblyProductAttribute("2-new_project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("2-new_project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
